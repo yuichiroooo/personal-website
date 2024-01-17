a78ee0b5abb12888d59f6237a17b21b792444d45
yuichiro <zirogingin@gmail.com>
2024-01-17 19:08:09 +0900

    create footer

---
+++ src/app/_components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import { FaXTwitter, FaGithub } from "react-icons/fa6";
+import { twitterUrl, githubUrl, email } from "@/app/_constants/urls";
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="flex justify-center m-4">
+                <a href={twitterUrl} target="_blank" rel="noopener noreferrer" className="mr-6"><FaXTwitter size={35} /></a>
+                <a href={githubUrl} target="_blank" rel="noopener noreferrer"><FaGithub size={35} /></a>
+            </div>
+            <p className="flex justify-center mb-10">Email: {email}</p>
+        </footer>
+    )
+}
