7756165498d5ddf2106f72ced27f154b09859991
yuichiro <zirogingin@gmail.com>
2024-01-17 13:54:01 +0900

    create research page

---
+++ src/app/research/page.tsx
@@ -0,0 +1,7 @@
+import { Researches } from "../_constants/constants"
+
+export default function Research() {
+    return (
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    )
+}
