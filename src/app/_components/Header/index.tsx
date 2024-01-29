28c6608d658b131aed4f75cd0d26d8215a6145c2
yuichiro <zirogingin@gmail.com>
2024-01-30 00:41:46 +0900

    update

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -5,7 +5,7 @@ export default function Header() {
         <header className="font-sans">
             <div className="flex justify-between mt-12">
                 <h1 className="text-5xl font-bold" style={{ paddingLeft: "10%" }}>Yuichiro Kinoshita</h1>
-                <div className="flex text-3xl mt-4" style={{ marginRight: "20%" }}>
+                <div className="flex text-3xl mt-2" style={{ marginRight: "20%" }}>
                     <Link href="/" className='hover:text-red-400'><h2 className='mr-10'>Home</h2></Link>
                     <Link href="/research" className='hover:text-red-400'><h2>Research</h2></Link>
                 </div>
